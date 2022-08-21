c6fb00c51c062721e078930f6d00ea32772af457
ofplarsen <ofplarsen@gmail.com>
2022-08-21 14:56:54 +0200

    Now works for both a and b in same run

--- Oving1/main.cpp
+++ Oving1/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void read_temperatures(double temperatures[], int length);
 
 int main() {
-    const int length = 6;
+    const int length = 5;
     double temperatures[length];
     // Oving 1 a)
 
@@ -20,13 +20,32 @@ int main() {
         cin >> temperatures[i];
     }
 
-
-    //Oving 1 b)
-    read_temperatures(temperatures, length);
-
     int underTen = 0;
     int between = 0;
     int overTwenty = 0;
+
+    for(double temperature : temperatures){
+        if(temperature < 10)
+            underTen++;
+        else if(temperature >= 10 && temperature <= 20)
+            between++;
+        else if(temperature > 20)
+            overTwenty++;
+    }
+    cout << "Antall under 10 er " << underTen << endl;
+    cout << "Antall mellom 10 og 20 er " << between << endl;
+    cout << "Antall over 20 er " << overTwenty << endl;
+
+
+    //Oving 1 b)
+    cout << endl;
+    cout << endl;
+    cout << "Leser fra fil: " << endl;
+    read_temperatures(temperatures, length);
+
+    underTen = 0;
+    between = 0;
+    overTwenty = 0;
 
     for(double temperature : temperatures){
         if(temperature < 10)
@@ -61,4 +80,5 @@ void read_temperatures(double temperatures[], int length){
     }
 
 }
+
 
