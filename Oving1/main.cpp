7cc22c74bb6d65a6092408a91b318437b8c678fb
ofplarsen <ofplarsen@gmail.com>
2022-08-21 14:28:59 +0200

    Updated gitignore and done with O1 a)

--- Oving1/main.cpp
+++ Oving1/main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
-#include <cctype>
-#include <cstring>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include <iomanip>
 
 using namespace std;
+
+
 int main() {
-    char text[5];
-    cout << "Skriv inn et ord: ";
-    cin >> text;
+    // Oving 1 a)
 
-    for(int i = 0; i < strlen(text); i++){
-        text[i] = toupper(text[i]); //Uppercase
-    }
-    cout << "Uppercase: " << text << endl;
+    const int length = 5;
+    double temperatures[length];
+    cout << "Du skal skrive inn " << length << " temperaturer." << endl;
 
-    for(int j = 0; j < strlen(text); j++){
-        text[j] = tolower(text[j]);
+    for(int i = 0; i < length; i++){
+        cout << "Temperatur nr " << i + 1 << ": ";
+        cin >> temperatures[i];
     }
 
-    cout << "Lower case: " << text << endl;
+    int underTen = 0;
+    int between = 0;
+    int overTwenty = 0;
 
+    for(double temperature : temperatures){
+        if(temperature < 10)
+            underTen++;
+        else if(temperature >= 10 && temperature <= 20)
+            between++;
+        else if(temperature > 20)
+            overTwenty++;
+    }
+    cout << "Antall under 10 er " << underTen << endl;
+    cout << "Antall mellom 10 og 20 er " << between << endl;
+    cout << "Antall over 20 er " << overTwenty << endl;
     return 0;
 }
